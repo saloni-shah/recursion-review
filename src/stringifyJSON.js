633687cce5234f45723dd570ece69c7895a57c82
Hack Reactor Student <student@hackreactor.com>
2017-05-15 15:39:10 -0700

    Complete JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,45 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+const map = function(obj){
+  const results = [];
+  each(obj, (value) => {
+    results.push(stringifyJSON(value));
+  });
+  return '[' + results.join(',') + ']';
 };
+
+const each = function(obj, callback) {
+  if(Array.isArray(obj)){
+    for(let index =0; index < obj.length; index++){
+      callback(obj[index]);
+    }
+  } 
+};
+
+var stringifyJSON = function(obj) {
+  if (obj === null){
+    return 'null';
+  } else if (Array.isArray(obj)){
+    return map(obj, stringifyJSON);
+  } else if (obj && typeof obj === 'object'){
+    var results = [];
+    for (var key in obj) {
+      if (typeof obj[key] === 'function' || typeof obj[key] === 'undefined') {
+        continue;  
+      }
+      results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + results.join(',') + '}';
+  } else if(isFinite(obj)){
+    return String(obj);
+  } else if (typeof obj === 'boolean'){
+    return String(obj);
+  } else if (typeof obj === 'string'){
+    return '"'+obj+'"';
+  } else if (typeof obj === 'undefined'){
+    return obj;
+  } 
+};
+
+
