bdc8c00b47906cdb1b82288ec71f5e4ce250028d
Hack Reactor Student <student@hackreactor.com>
2017-05-15 19:09:32 -0700

    Complete getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,29 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  let nodesArr = nodes(document.body);
+  let classElems = [];
+  for(let i = 0; i < nodesArr.length; i++){
+    if(nodesArr[i].nodeName !== '#text'){
+      if(nodesArr[i].className!==undefined && nodesArr[i].className.indexOf(className) !== -1){
+        classElems.push(nodesArr[i]);
+      }
+    }
+  }
+  return classElems;
+};
+
+const visitAllNodes = function(node, callback) {
+  if (node.childNodes) {
+    callback(node);
+    node.childNodes.forEach( (value) => visitAllNodes(value,callback));
+  }
+};
+
+const nodes = function(node) {
+  let nodesArr = [];
+  visitAllNodes(node, (currentNode) => nodesArr.push(currentNode));
+  return nodesArr;
 };
